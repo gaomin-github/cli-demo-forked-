cf993578e97052a413114cfcfcf2f0c5cc21bf46
gaomin.5 <gaomin.5@bytedance.com>
2023-11-10 11:11:19 +0800

    fix(lerna-version): 修改tool方法来验证版本更新

--- packages/tools/src/utils/tools.ts
+++ packages/tools/src/utils/tools.ts
@@ -12,14 +12,10 @@ const printLog = (log: string) => {
   console.log(`printLog：${log}`);
 };
 
-const request = () => {
-  console.log("start request");
-};
 
 const tools = {
   reportException,
   printLog,
-  request,
 };
 
 export default tools;
