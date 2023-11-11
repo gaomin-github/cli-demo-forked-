69f3993c89d9561bdbb1abbf9182bafa31a15dd8
gaomin.5 <gaomin.5@bytedance.com>
2023-11-11 15:32:27 +0800

    fix(packages): 增加packages|tools方法

--- packages/tools/src/utils/tools.ts
+++ packages/tools/src/utils/tools.ts
@@ -12,10 +12,14 @@ const printLog = (log: string) => {
   console.log(`printLog：${log}`);
 };
 
+const printError = (log: string) => {
+  console.error(`printLog：${log}`);
+};
 
 const tools = {
   reportException,
   printLog,
+  printError,
 };
 
 export default tools;
